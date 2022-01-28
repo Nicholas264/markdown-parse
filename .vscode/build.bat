ac07a881441352d7443047fc0946e23670f1dae8
Nick <nick-ls@users.noreply.github.com>
2022-01-28 12:16:59 -0800

    Add JUnit files

---
+++ .vscode/build.bat
@@ -0,0 +1,4 @@
+REM C:\Users\Nick\.vscode\extensions\redhat.java-1.3.0\jre\17.0.1-win32-x86_64\bin\javac.exe -cp ".;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar" MarkdownParseTest.java MarkdownParse.java
+REM C:\Users\Nick\.vscode\extensions\redhat.java-1.3.0\jre\17.0.1-win32-x86_64\bin\java.exe -cp ".;.\lib\junit-4.13.2.jar;.\lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+C:\Users\Nick\.vscode\extensions\redhat.java-1.3.0\jre\17.0.1-win32-x86_64\bin\javac.exe MarkdownParse.java
+C:\Users\Nick\.vscode\extensions\redhat.java-1.3.0\jre\17.0.1-win32-x86_64\bin\java.exe MarkdownParse empty-link.md
